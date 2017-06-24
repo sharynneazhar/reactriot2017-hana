92ecaa26e3ea749597edf2643de1e38ad0a4aad2
Sharynne Azhar <sharynneazhar@gmail.com>
2017-06-24 18:56:11 -0500

    use bootstrap list group component

--- src/components/Job/index.js
+++ src/components/Job/index.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
+import { ListGroupItem } from 'react-bootstrap';
 import './styles.css';
 
 class Job extends Component {
   render() {
     return (
-      <div>
+      <ListGroupItem className="job-container">
         <h4>{this.props.job.title}</h4>
         <p>{this.props.job.createdAt}</p>
         <p>{this.props.job.description}</p>
         <p>{this.props.job.compensation}</p>
-      </div>
+      </ListGroupItem>
     );
   }
 }
--- src/containers/JobListContainer.js
+++ src/containers/JobListContainer.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { Row, Col, Glyphicon } from 'react-bootstrap';
+import { Row, Col, ListGroup } from 'react-bootstrap';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 import SearchBox from '../components/SearchBox';
@@ -22,15 +21,15 @@ class JobListContainer extends Component {
   }
 
   render() {
-    console.log(this.props);
     return (
       <Row>
         <Col md={4}>
-          <Link to='/create'><Glyphicon glyph="plus" /> New Post</Link>
           <SearchBox />
         </Col>
         <Col md={8}>
-          {this.props.data.loading ? <p>Loading...</p> : this._renderPosts()}
+          <ListGroup>
+            {this.props.data.loading ? <p>Loading...</p> : this._renderPosts()}
+          </ListGroup>
         </Col>
       </Row>
     );
