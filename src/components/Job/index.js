9f110dcb562412fc0e875fd3ce406941244a4bcc
Sharynne Azhar <sharynneazhar@gmail.com>
2017-06-25 06:36:34 -0500

    style job list view a bit

--- src/components/Job/index.js
+++ src/components/Job/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { ListGroupItem } from 'react-bootstrap';
+import { ListGroupItem, Row, Col, Glyphicon } from 'react-bootstrap';
+import moment from 'moment'
 import './styles.css';
 
 class Job extends Component {
@@ -12,11 +13,23 @@ class Job extends Component {
 
     return (
       <ListGroupItem className="job-container">
-        <h4>{job.title}</h4>
-        <p>{job.createdAt}</p>
-        <p>{job.description}</p>
-        <p>{job.location ? job.location.name : 'Contact job poster for location.'}</p>
-        <p>{job.compensation}</p>
+        <div className="title">{job.title}</div>
+        <Row>
+          <Col md={3}>
+            <div className="date">
+              <Glyphicon glyph="calendar" />
+              Posted {moment(job.createdAt).fromNow()}
+            </div>
+          </Col>
+          <Col md={9}>
+            <div className="location">
+              <Glyphicon glyph="map-marker" />
+              {job.location ? `Located in ${job.location.name}` : 'Contact job poster for location.'}
+            </div>
+          </Col>
+        </Row>
+        <div className="desc">{job.description}</div>
+        <div className="compensation">{job.compensation}</div>
       </ListGroupItem>
     );
   }
